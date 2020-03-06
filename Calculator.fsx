13d036d14e0d65b245336f6d8827b89098899eb7
Sebastian Lund Jensen <37780699+SebastianLJ@users.noreply.github.com>
2020-03-06 10:16:38 +0100

    Add files via upload

---
+++ Calculator.fsx
@@ -0,0 +1,69 @@
+// This script implements our interactive calculator
+
+// We need to import a couple of modules, including the generated lexer and parser
+#r "FsLexYacc.Runtime.10.0.0/lib/net46/FsLexYacc.Runtime.dll"
+open FSharp.Text.Lexing
+open System
+#load "CalculatorTypesAST.fs"
+open CalculatorTypesAST
+#load "CalculatorParser.fs"
+open CalculatorParser
+#load "CalculatorLexer.fs"
+open CalculatorLexer
+
+// We define the evaluation function recursively, by induction on the structure
+// of arithmetic expressions (AST of type expr)
+let rec eval e =
+  match e with
+    | AssignExpr(x,y) -> //todo
+    | SeperatorExpr(x,y) -> //todo
+    | IfExpr(x) -> //todo
+    | DoExpr(x) -> //todo
+
+    | FuncExpr -> //todo
+    | ConcExpr -> //todo
+
+    | N(x) -> x
+    | X(x) -> x //return value of x?
+    | PlusExpr(x,y) -> eval(x) + eval (y)
+    | MinusExpr(x,y) -> eval(x) - eval (y)
+    | TimesExpr(x,y) -> eval(x) * eval (y)
+    | DivExpr(x,y) -> eval(x) / eval (y)
+    | PowExpr(x,y) -> eval(x) ** eval (y)
+    | UMinusExpr(x) -> - eval(x)
+
+    | Tf(x) -> x //return true/false
+    | AndhExpr(x,y) -> eval(x) && eval(x)
+    | OrhExpr(x,y) -> eval(x) || eval(x)
+    | NotExpr(x) -> not eval(x)
+    | EqualExpr(x,y) -> eval(x) = eval(y)
+    | NequalExpr(x,y) -> eval(x) <> eval(y)
+    | GtExpr(x,y) -> eval(x) > eval(y)
+    | GteExpr(x,y) -> eval(x) >= eval(y)
+    | LtExpr(x,y) -> eval(x) < eval(y)
+    | LteExpr(x,y) -> eval(x) <= eval(y)
+
+let parse input =
+    // translate string into a buffer of characters
+    let lexbuf = LexBuffer<char>.FromString input
+    // translate the buffer into a stream of tokens and parse them
+    let res = CalculatorParser.start CalculatorLexer.tokenize lexbuf
+    // return the result of parsing (i.e. value of type "expr")
+    res
+
+// We implement here the function that interacts with the user
+let rec compute n =
+    if n = 0 then
+        printfn "Bye bye"
+    else
+        printf "Enter an arithmetic expression: "
+        try
+        // We parse the input string
+        let e = parse (Console.ReadLine())
+        // and print the result of evaluating it
+        printfn "Result: %f" (eval(e))
+        compute n
+        with err -> printfn "invalid syntax"
+
+// Start interacting with the user
+compute 3
---
+++ CalculatorTypesAST.fs
@@ -0,0 +1,37 @@
+// This file implements a module where we define a data type "expr"
+// to store represent arithmetic expressions
+module CalculatorTypesAST
+
+ type C =
+  | AssignExpr of (X * a)
+  | SeperatorExpr of (C * C)
+  | IfExpr of (GC)
+  | DoExpr of (GC)
+
+ type GC =
+  | FuncExpr of (b * C)
+  | ConcExpr of (GC * GC)
+
+ type a = 
+  | N of int
+  | X of string
+  | PlusExpr of (a * a)
+  | MinusExpr of (a * a)
+  | TimesExpr of (a * a)
+  | DivExpr of (a * a)
+  | UMinusExpr of (a)
+  | PowExpr of (a * a)
+
+ type b =
+  | Tf of bool
+  | AndExpr of (b * b)
+  | OrExpr of (b * b)
+  | AndHardExpr of (b * b)
+  | OrHardExpr of (b * b)
+  | NotExpr of (b)
+  | EqualExpr of (b * b)
+  | NEqualExpr of (b * b)
+  | GtExpr of (b * b)
+  | GteExpr of (b * b)
+  | LtExpr of (b * b)
+  | LteExpr of (b * b)
