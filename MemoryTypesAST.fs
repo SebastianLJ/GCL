c754a130c1969217faaf5115621584640a6b09b9
Emil <s184185@student.dtu.dk>
2020-04-22 16:49:04 +0200

    Renamed files and variable names. Cleaned up code.

---
+++ MemoryTypesAST.fs
@@ -0,0 +1,25 @@
+﻿module MemoryTypesAST
+open System
+
+type ConArrContent =
+  | ConNum of int
+  | ConNums of int * ConArrContent
+
+type ConElement =
+  | ConVar of string * int
+  | ConArr of char * ConArrContent
+  | ConSeq of ConElement * ConElement
+
+type AbsArrContent =
+  | Sign of char
+  | Signs of char * AbsArrContent
+  
+type AbsElement =
+  | AbsVar of string * char
+  | AbsArr of char * AbsArrContent
+  | AbsSeq of AbsElement * AbsElement
+  
+type Memory =
+  | AbstractMemory of AbsElement
+  | ConcreteMemory of ConElement
+
